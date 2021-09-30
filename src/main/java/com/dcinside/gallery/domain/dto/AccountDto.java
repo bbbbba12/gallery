797f0d7ba6ba13b1765ef370546607844951b0f8
bknote <qjsro742@gmail.com>
2021-09-30 09:40:23 +0900

    first commit

--- src/main/java/com/dcinside/gallery/domain/dto/AccountDto.java
+++ src/main/java/com/dcinside/gallery/domain/dto/AccountDto.java
@@ -1,8 +1,11 @@
 package com.dcinside.gallery.domain.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor @AllArgsConstructor
 public class AccountDto {
     private String username;
     private String password;
